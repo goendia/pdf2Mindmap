83041b45d685e5747ba8c6b8761a177735830de8
goendia <goendia@posteo.de>
2025-06-07 08:34:13 +0200

    Remove docling python file

--- docling.py
+++
@@ -1,7 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-source = "1_DOCLING_1-2_CompTIA+Network++(N10-009)+Study+Guide.pdf" \
-""  # PDF path or URL
-converter = DocumentConverter()
-result = converter.convert(source)
-print(result.document.export_to_dict())  # output: "### Docling Technical Report[...]"
